9792912df1c5cbce6d1652a1756e37e9669dfe99
Prafull-Mhn <prafull.mhn@outlook.com>
2025-02-25 11:13:10 +0530

    Updated render detail

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -9,7 +9,7 @@ document.getElementById('appointmentForm').addEventListener('submit', function (
     const date = document.getElementById('date').value;
     const time = document.getElementById('time').value;
 
-    fetch('https://paisapay-dev.algoskytech.com/api/Common/sendEmail', {
+    fetch('https://appointment-skc5.onrender.com/send-email', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
